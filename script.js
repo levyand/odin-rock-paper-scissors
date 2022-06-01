e01b1d1ca4d48623b6d5a7e206f35f222c0fb41d
Andrew Levy <levyand@gmail.com>
2022-06-01 16:18:47 -0500

    Update: makes many changes detailed below
    Adds a playerPlay function that loops until the player makes
    a valid selection.
    
    Changes the return value of both computerPlay and playerPlay to
    numeric values that represent the index of the selection in
    POSSIBLE_PLAYS.
    
    Simplifies how the winner of a round is determined and what
    message about the round is returned with the results.
    
    Makes WIN and LOSE results constant for less error prone comparisons.
    
    Combines player and computer scores in game into a single score
    state object.

--- script.js
+++ script.js
@@ -1,73 +1,83 @@
-const POSSIBLE_PLAYS = ['rock', 'paper', 'scissors'];
+const POSSIBLE_PLAYS = ['Rock', 'Paper', 'Scissors'];
+const POSSIBLE_MESSAGES = {
+  '00': 'You both chose Rock.',
+  '01': 'Paper covers Rock.',
+  '02': 'Rock crushes Scissors.',
+  '11': 'You both chose Paper.',
+  '12': 'Scissors cut Paper.',
+  '22': 'You both chose Scissors.'
+};
+const RESULT_WIN = 'You Win!';
+const RESULT_LOSE = 'You Lose!';
 
+// Returns a random number between 0 and 2 representing the computers selection
 function computerPlay() {
-  const rand = Math.floor(Math.random() * 3);
-  
-  return POSSIBLE_PLAYS[rand];
+  return Math.floor(Math.random() * 3);
 }
 
-function playRound(playerSelection, computerSelection) {
-  const player = playerSelection.toLowerCase();
-  const computer = computerSelection.toLowerCase();
+// Returns a number representing the players selection
+function playerPlay() {
+  const options = POSSIBLE_PLAYS.reduce((prev, curr) => `${prev}, ${curr}`);
+  
+  let player = -1;
 
-  switch (player) {
-    case ('rock'):
-      switch (computer) {
-        case ('rock'):
-          return 'Tie Game! You both chose Rock';
-        case ('paper'):
-          return 'You Lose! Paper covers Rock';
-        case ('scissors'):
-          return 'You Win! Rock crushes Scissors';
-      }
-    case ('paper'):
-      switch (computer) {
-        case ('rock'):
-          return 'You Win! Paper covers Rock';
-        case ('paper'):
-          return 'Tie Game! You both chose Paper';
-        case ('scissors'):
-          return 'You Lose! Scissors cut Paper';
-      }
-    case ('scissors'):
-      switch (computer) {
-        case ('rock'):
-          return 'You Lose! Rock crushes Scissors';
-        case ('paper'):
-          return 'You Win! Scissors cut Paper';
-        case ('scissors'):
-          return 'Tie Game! You both chose Scissors';
-      }
-    default:
-      return `'${playerSelection}' is not a valid option.`;
+  do {
+    const playerInput = prompt(`${options}... SHOOT!.`);
+    player = POSSIBLE_PLAYS.findIndex(val => val.toLowerCase() === playerInput.toLowerCase());
+
+    if (player < 0) { console.log(`'${playerInput}' is not a valid option. Try again.`); }
+  } while (player < 0);
+
+  return player;
+}
+
+// Returns results of a single round
+function playRound(playerSelection, computerSelection) {
+  // Generate a message based on the selections and the possible outcomes
+  const messageKey = playerSelection > computerSelection ? 
+    `${computerSelection}${playerSelection}` : 
+    `${playerSelection}${computerSelection}`;
+    console.log(messageKey);
+  const message = POSSIBLE_MESSAGES[messageKey];
+
+  // Determine who won and return the result with an additional message
+  if (playerSelection === computerSelection) {
+    return `Draw! ${message}`;
+  } else if (((playerSelection + 1) % 3) === computerSelection) {
+    return `${RESULT_LOSE} ${message}`;
+  } else {
+    return `${RESULT_WIN} ${message}`;
   }
 }
 
+// Plays a game of 5 rounds
 function game() {
-  let playerScore = 0;
-  let computerScore = 0;
+  let score = {
+    player: 0,
+    computer: 0
+  };
 
   for (let i = 0; i < 5; i++) {
-    const player = prompt('Make your selection.');
+    const player = playerPlay();
     const computer = computerPlay();
 
     const results = playRound(player, computer);
 
     console.log(`Round ${i + 1}: ${results}`);
 
-    if (results.startsWith('You Win!')) {
-      playerScore++;
-    } else if (results.startsWith('You Lose!')) {
-      computerScore++;
+    if (results.startsWith(RESULT_WIN)) {
+      score.player++;
+    } else if (results.startsWith(RESULT_LOSE)) {
+      score.computer++;
     }
   }
 
-  console.log(`Final Score\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
+  console.log(`Final Score\nPlayer: ${score.player}\nComputer: ${score.computer}`);
 
-  if (playerScore > computerScore) {
+  if (score.player === score.computer) {
+    console.log("It's a draw.");
+  } else if (score.player > score.computer) {
     console.log('Congratulations, you win!');
-  } else if (playerScore === computerScore) {
-    console.log('You tied.');
   } else {
     console.log('Better luck next time, you lose.');
   }
